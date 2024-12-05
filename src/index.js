35fbb05e4faa59671b1f5e4abb772639ac981fc0
Abhishek <abhishektiwari4843@gmail.com>
2024-12-06 01:54:12 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDom.render(
+//   <>
+//     <h1> List of 5 best web Series</h1>
+//     <ol>
+//       <li>Breading bed</li>
+//       <li>Money Heist</li>
+//       <li>Game of Thorones</li>
+//       <li>Family man</li>
+//     </ol>
+//   </>,
+//   document.getElementById('root')
+// )
+
+//expression
+// const fname= "ab";
+// ReactDom.render(
+//   <>
+//     <h1>my first name is {fname}</h1>
+//     <p> adding numbeer {2+5}</p>
+//   </>,
+//   document.getElementById('root')
+// )
+
+//template literrals
+
+// const fname="ab";
+// const lname="Ti";
+// ReactDom.render(
+//   <>
+//    <h1>my name is {`${fname} ${lname}`}</h1>
+//   </>,
+//   document.getElementById('root')
+// )
+
+
+// challenge 2 
+
+const name= "abhisek";
+ const currDate= new Date().toLocaleDateString();
+ const currTime = new Date().toLocaleTimeString();
+
+ReactDom.render(
+  <>
+    <h1>Hello,  my name is {name}</h1>
+    <p> Today current date is {currDate}</p>
+    <p> current time is {currTime}</p>
+   
+  </>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
