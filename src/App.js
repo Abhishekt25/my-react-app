0b00450c50fc5cb0ea8b296630aab5522b8f52df
Abhishek Tiwari <abhishektiwari4843@gmail.com>
2024-12-05 19:30:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App(){
+  return(
+    <div className="App">
+      <header className='App-header'>
+        <h2>This is my first code for React.Js</h2>
+      </header>
+    </div>
+  )
+}
+
+export default App;
